2a51ca83e258232a64080559e5d9f38b3e6b6451
Michael-Riv <Michriv@umich.edu>
2025-08-07 17:43:41 -0400

    Update:
    Finish side navigation bar for home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,10 @@
 'use client';
 import Image from "next/image";
+import SideBarNav from "@/components/navBars/side";
 export default function Home() {
   return (
     <div>
+      <SideBarNav/>
     
     </div>
   );
--- frontend/src/components/navBars/side.jsx
+++ frontend/src/components/navBars/side.jsx
@@ -1,6 +1,47 @@
-export default function sideBarNav(){
+'use client';
+import Link from 'next/link';
+
+
+
+
+
+export default function SideBarNav(){
+
+
+   const handleLogout=()=>{
+    //do something here if user logged in,in this case there is no log in
+    console.log('user logged out');
+    }
+
+    const menuItems=[
+        {label: 'Tasks',path:'#'},
+        {label: 'Calendar',path:'#'},
+        {label: 'Logout',action:handleLogout},
+    ]
+
+
+
     return(
-        <div>
+        <div className='h-screen w-60 bg-gray-600 text-white flex flex-col justify-between px-8 py-6'>
+            <div>
+                <h1 className='text-2xl font-bold mb-6 border-b border-amber-400 pb-1'>Task Manager</h1>
+                <nav className='space-y-4'>
+                    {menuItems.map((item)=>
+                    item.action ? (
+                    <button key={item.label} onClick={item.action} className='block text-left w-full border border-white rounded hover:bg-blue-600 hover:text-black pl-2'>
+                        {item.label}
+                    </button>
+                    ): (
+                    <Link key={item.label} href={item.path} className='block border w-full border-white rounded hover:bg-blue-600 hover:text-black pl-2'>
+                        {item.label}
+                    </Link>
+                    ))}
+                </nav>
+            </div>
+
+            <div className='text-2xl text-white'>
+                    Welcome, User
+            </div>
 
         </div>
     );
